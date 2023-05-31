2070c29c3c03d4b3e2887755dbe312688602a330
abhiram_gummula <argummula2000@gmail.com>
2023-05-31 16:02:41 +0530

    improved scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -33,7 +33,7 @@ const NavBar = () => {
       </div>
       <ul className='hidden md:flex'>
         {links.map(({id, link})=> <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200' key={id}>
-          <Link to={link} smooth offset={-80} duration={500}>{link}</Link>
+          <Link to={link} smooth offset={-80} duration={1000}>{link}</Link>
           </li>)}
       </ul>
       <div className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden' onClick={()=>setNav(!nav)}>
@@ -42,7 +42,7 @@ const NavBar = () => {
       {nav && (<ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
         {links.map(({id, link})=> (
         <li className="px-4 cursor-pointer capitalize py-6 text-4xl" key={id}>
-          <Link onClick={()=>{setNav(!nav)}} to={link} smooth duration={500}>{link}</Link>
+          <Link onClick={()=>{setNav(!nav)}} to={link} smooth duration={1000}>{link}</Link>
           </li>))}
       </ul>)}
     </div>
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -34,7 +34,7 @@ const SocialLinks = () => {
     child: (
       <>Resume  <BsFillPersonLinesFill size={30}/></>
     ),
-    href: '/ABHIRAM_G_FE.pdf',
+    href: process.env.PUBLIC_URL + '/ABHIRAM_G_FE.pdf',
     style: 'rounded-br-md',
     download: true,
   },
